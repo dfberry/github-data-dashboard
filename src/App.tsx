29db8ecd31fafb155761fbba439add3effd4de68
4ttedertlf <107864453+4ttedertlf@users.noreply.github.com>
2023-01-06 17:05:44 -0800

    table works

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,109 @@
 import React from 'react';
 import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+import { useTable } from 'react-table';
+
+const Styles = styled.div`
+  padding: 1rem;
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+
+
+function Table({ columns, data }:any) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  })
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
 
 function App() {
+
+  const data = [
+    {col1: "hello", col2:"goodbye"},
+    {col1: "dog", col2:"cat"},
+    {col1: "blue", col2:"green"},
+  ]
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'Col1',
+            accessor: 'col1',
+          },
+          {
+            Header: 'Col2',
+            accessor: 'col2',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Styles>
+      <Table columns={columns} data={data} />
+      </Styles>
     </div>
   );
 }
