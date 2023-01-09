0fee76035693a72f62e5d7f73f5cc51e66b39a58
4ttedertlf <107864453+4ttedertlf@users.noreply.github.com>
2023-01-08 17:10:28 -0800

    Remove nav

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,8 @@
-import Navigation from './Navigation'
 import Footer from './Footer'
 
 const Layout = ({ children }:any) => {
   return (
     <>
-      <Navigation />
       <main>{children}</main>
       <Footer />
     </>
