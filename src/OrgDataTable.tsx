7eeed239c0d125f3027577e03b025ac46b5f080f
4ttedertlf <107864453+4ttedertlf@users.noreply.github.com>
2023-01-24 07:01:09 -0800

    filter repo table

--- src/OrgDataTable.tsx
+++ src/OrgDataTable.tsx
@@ -1,7 +1,11 @@
 // @ts-nocheck
+import { useMemo } from "react";
 import { Link } from "react-router-dom";
-import { useTable, useSortBy } from "react-table";
+import { useTable, useSortBy, useFilters } from "react-table";
 import styled from "styled-components";
+
+// filters
+import { TextSearchFilter } from "./utilities/filters";
 
 const Styles = styled.div`
   padding: 1rem;
@@ -32,101 +36,116 @@ const Styles = styled.div`
   }
 `;
 function Table({ columns, data }: any) {
-    // Use the state and functions returned from useTable to build your UI
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-      useTable({
+
+  const defaultColumn = useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: ""
+    }),
+    []
+  );
+
+
+  // Use the state and functions returned from useTable to build your UI
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable(
+      {
         columns,
         data,
+        defaultColumn,
       },
-      useSortBy);
-  
-    // Render the UI for your table
-    return (
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>{
-                  column.render("Header")
-                  }
-                  <span>
-                    {column.isSorted
-                      ? column.isSortedDesc
-                        ? ' 🔽'
-                        : ' 🔼'
-                      : ''}
-                  </span>
-                  </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return <td {...cell.getCellProps()}>{cell.column.id === 'rowNumber' ? i + 1 : cell.render('Cell')}</td>;
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      useFilters,
+      useSortBy
+
     );
-  }
- 
-  function DataTableOrg({data, collectionDate}:any): JSX.Element{
 
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                {column.render("Header")}
+                <span>
+                  {column.isSorted ? (column.isSortedDesc ? " 🔽" : " 🔼") : ""}
+                </span>
+                <div></div>
+                <div>{column.canFilter ? column.render("Filter") : null} </div>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <td {...cell.getCellProps()}>
+                    {cell.column.id === "rowNumber"
+                      ? i + 1
+                      : cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
 
-    const columns = [
-      {
-        Header: `${collectionDate} - ${data.length} repos`,
-        columns: [
-          {
-            Header: "#",
-            id: 'rowNumber',
+function DataTableOrg({ data, collectionDate }: any): JSX.Element {
+  const columns = [
+    {
+      Header: `${collectionDate} - ${data.length} repos`,
+      columns: [
+        {
+          Header: "#",
+          id: "rowNumber",
+        },
+        {
+          Header: "Stars",
+          accessor: "stars",
+        },
+        {
+          Header: "Forks",
+          accessor: "forks",
+        },
+        {
+          Header: "Repo",
+          accessor: "repositoryName",
+          Filter: TextSearchFilter,
+          Cell: (row: any) => {
+            const repoName = row.cell.value;
+            if (!repoName) return "";
+            const url = `/repo?name=${repoName}&owner=${"Azure-samples"}`;
+            return <Link to={url}>{repoName}</Link>;
           },
-          {
-            Header: "Stars",
-            accessor: "stars",
-          },
-          {
-            Header: "Forks",
-            accessor: "forks",
-          },
-          {
-            Header: "Repo",
-            accessor: "repositoryName",
-            Cell: (row: any) => {
+        },
 
-              const repoName = row.cell.value;
-              if (!repoName) return "";
-              const url = `/repo?name=${repoName}&owner=${'Azure-samples'}`;
-              return (<Link to={url} >{repoName}</Link>)
-            }
-          },
-  
-          {
-            Header: "Issues",
-            accessor: "issues",
-          },
-          {
-            Header: "PRs",
-            accessor: "pr",
-          },
-        ],
-      },
-    ];
-  
+        {
+          Header: "Issues",
+          accessor: "issues",
+        },
+        {
+          Header: "PRs",
+          accessor: "pr",
+        },
+      ],
+    },
+  ];
 
-    return (
-          <Styles>
-              <Table columns={columns} data={data} />
-          </Styles>
-      );
-  }
+  return (
+    <Styles>
+      <Table columns={columns} data={data} />
+    </Styles>
+  );
+}
 
-  export default DataTableOrg;
+export default DataTableOrg;
